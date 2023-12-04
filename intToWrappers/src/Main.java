c8ea98bd0d4a573c202d8136489edbe96602ed36
JuniorTemp <junior_sadaanzinhoa@hotmail.com>
2023-12-04 15:57:00 -0300

    first commit

---
+++ intToWrappers/src/Main.java
@@ -0,0 +1,36 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        int numero;
+        char c;
+        double d;
+
+        // Integer
+     numero = 10;
+        System.out.println(numero);
+
+        Integer numero2 = numero;
+
+     System.out.println(numero2);
+     //Character
+        c = 'A';
+        System.out.println(c);
+
+        Character cc =(char) c;
+        System.out.println(cc);
+
+        //Double
+
+        d = 12.222;
+        System.out.println(d);
+        Double d2 = d;
+        System.out.println(d2);
+
+
+
+
+
+
+    }
+}
